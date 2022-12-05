53962a810dab4bc55ef6e913f09c72e0c19b941e
EnderF-R <119357812+EnderF-R@users.noreply.github.com>
2022-12-05 11:44:26 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+hummingbird.startHummingbird()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    if (hummingbird.getSensor(SensorType.Light, ThreePort.One) < 10 || hummingbird.getSensor(SensorType.Dial, ThreePort.Two) > 1) {
+        hummingbird.setPositionServo(FourPort.One, 0)
+    } else {
+        hummingbird.setPositionServo(FourPort.One, 180)
+        basic.pause(300)
+        hummingbird.setPositionServo(FourPort.One, 20)
+        basic.pause(300)
+    }
+})
+basic.forever(function () {
+    if (hummingbird.getSensor(SensorType.Light, ThreePort.One) < 10) {
+        hummingbird.setPositionServo(FourPort.Two, 0)
+    } else {
+        hummingbird.setPositionServo(FourPort.Two, 180)
+    }
 })
